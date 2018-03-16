bc89bb2ad2a56efcd62f21fd0c422ae248af768e
Rich Smith <rich@richjdsmith.ca>
2018-03-16 14:49:27 +0100

    Changed to match local copy giving errors.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   
-  authenticated :user do
-    root to: 'dashboards#index', as: :authenticated_root
-  end
+  # authenticated :user do
+  #   root to: 'dashboards#index', as: :authenticated_root
+  # end
   root 'static_pages#index'
   resources :static_pages
-  resources :dashboards, as: :dashboard
-  get 'dashboard', to: 'dashboards#index'
+  resource :dashboard
   get 'home', to: 'static_pages#index', as: :home
 
   # This is the equivalent of doing devise_scope :user do - it matches the paths to devise routes
